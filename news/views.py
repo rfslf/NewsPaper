79e111b6e73148b19ea555b9c208b80eb61035dc
Stanislav A <stanislav@avdonin.me>
2023-04-24 16:49:36 +0300

    View added
    posts.html and post.html extends flatpages/default.html
    Need to test

--- news/views.py
+++ news/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.views.generic import ListView, DetailView  # импортируем класс, который говорит нам о том, \
+# что в этом представлении мы будем выводить список объектов из БД
+from .models import Post
 
-# Create your views here.
+
+class PostsList(ListView):
+	model = Post  # указываем модель, объекты которой мы будем выводить
+	template_name = 'posts.html'  # указываем имя шаблона, в котором будет лежать HTML, в нём будут все инструкции о том, как именно пользователю должны вывестись наши объекты
+	context_object_name = 'news'  # это имя списка, в котором будут лежать все объекты, его надо указать, чтобы обратиться к самому списку объектов через HTML-шаблон
+
+
+# создаём представление, в котором будут детали конкретного отдельного товара
+class PostDetail(DetailView):
+	model = Post # модель всё та же, но мы хотим получать детали конкретно отдельного товара
+	template_name = 'post.html' # название шаблона будет product.html
+	context_object_name = 'post' # название объекта
